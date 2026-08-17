f3e95658faeeaac129ea3c83ca624695aed33cf3
PANCHAYUBHANU <89331807+PANCHAYU-BHANU@users.noreply.github.com>
2026-08-17 12:28:20 +0530

    fix some error

--- services.py
+++ services.py
@@ -42,11 +42,14 @@ class AttendanceManager:
         conn.close()
 
     def parse_students_text(self, xml_file):
-        return self.engine.parse_student_file(xml_file)
+        from roster_parser import RosterParser
+        return RosterParser.load(xml_file)
 
     def process_image_web(self, image_path):
-        steps, _ = self.engine.process_and_analyze(image_path, [{"index": "1", "name": "Dummy"}], "legacy")
-        return None, steps
+        from signature_processor import SignatureSheetProcessor
+        processor = SignatureSheetProcessor()
+        binarized, steps = processor.process(image_path)
+        return binarized, steps
 
     def analyze_attendance_web(
         self,
